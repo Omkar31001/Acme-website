2c96915e8e75535305633aa565ac55efa6506f1f
Lucifer__ <ayush.199302185@muj.manipal.edu>
2020-10-01 23:42:27 +0530

    learning git

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
 
-$('#view-work').on('click', function() {
-  const images = $('#images').position().top;
-
-  $ ('html, body').animate(
-    {
-      scrollTop: images
-    },
-    900
-  );
+    $('html, body').animate(
+        {
+            scrollTop: images,
+        },
+        900
+    );
 });
-
-  
