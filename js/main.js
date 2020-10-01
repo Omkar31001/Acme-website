b03075fedfc757f9beb91d04cfe3c50615222c9b
Omkar Dave <omkar.ghanu18@gmail.com>
2020-10-02 01:16:25 +0530

    hello

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
+$('#view-work').on('click', function() {
+  const images = $('#images').position().top;
 
-// $('#view-work').on('click', function() {
-//   const images = $('#images').position().top;
-
-//   $ ('html, body').animate(
-//     {
-//       scrollTop: images
-//     },
-//     900
-//   );
-// });
+  $ ('html, body').animate(     {
+       scrollTop: images
+     },
+     900
+   );
+ });
 
   
